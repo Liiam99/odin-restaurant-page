6ba6b2489f5521fb51b677f3f0830337aa2700af
Liam <liam99mail@proton.me>
2025-05-01 12:52:27 +0200

    Add initial page load

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import "./styles.css";
 import loadHomeContent from "./home";
 
-const content = document.querySelector('#content');
-const home = loadHomeContent();
-content.appendChild(home);
+const contentContainer = document.querySelector('#content');
+
+const homeButton = document.querySelector('.home-button');
+homeButton.addEventListener('click', (e) => {
+    loadPage(loadHomeContent);
+    document.querySelector('.active-button').classList.remove('active-button');
+    e.target.classList += ' active-button';
+});
+
+
+function loadPage(loadContent) {
+    contentContainer.innerHTML = '';
+    const content = loadContent();
+    contentContainer.appendChild(content);
+}
+
+// Initial page load.
+loadPage(loadHomeContent);
